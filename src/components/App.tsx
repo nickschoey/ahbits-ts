4bc9f87b5fc2bdeafd2db86b901f1e4eeffdf6c3
Jordi M. Zambrano <nickschoey@gmail.com>
2020-03-08 11:48:17 +0100

    lesson 268

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { fetchTodos, Todo } from '../actions/index';
+import { StoreState } from '../reducers/index';
+import { connect } from 'react-redux';
 
-export class App extends React.Component {
+interface AppProps {
+  todos: Todo[];
+  fetchTodos(): any;
+}
+
+class _App extends React.Component<AppProps> {
+  componentDidMount() {}
+
   render() {
     return <div>Hello</div>;
   }
 }
+
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+  return { todos };
+};
+
+export const App = connect(mapStateToProps, { fetchTodos })(_App);
