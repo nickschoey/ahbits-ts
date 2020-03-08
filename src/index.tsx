da2f8118f17e479ef6195cabe7272d4862f1d73f
Jordi M. Zambrano <nickschoey@gmail.com>
2020-03-08 11:13:30 +0100

    lesson 263

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-interface AppProps {
-  color?: string;
-}
-
-const App = (props: AppProps): JSX.Element => {
-  return <div>{props.color}</div>;
-};
-
-// class App extends React.Component<AppProps> {
-//   state = { counter: 0 };
-
-//   onIncrement = (): void => {
-//     this.setState({ counter: this.state.counter + 1 });
-//   };
-
-//   onDecAppStaterement = (): void => {
-//     this.setState({ counter: this.state.counter - 1 });
-//   };
-//   render() {
-//     return (
-//       <div>
-//         <button onClick={this.onIncrement}>Increment</button>
-//         <button onClick={this.onDecrement}>Decrement</button>
-//         {this.state.counter}
-//       </div>
-//     );
-//   }
-// }
-
-ReactDOM.render(<App color="red" />, document.querySelector('#root'));
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { App } from './components/App';
+import { reducers } from './reducers';
+const store = createStore(reducers, applyMiddleware(thunk));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
