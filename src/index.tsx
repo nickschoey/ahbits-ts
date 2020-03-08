7e1e399f23308bdbaad0fc75c4d5ecf2fb987eb4
Jordi M. Zambrano <nickschoey@gmail.com>
2020-03-08 11:19:07 +0100

    lesson 264

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { App } from './components/App';
 import { reducers } from './reducers';
+
 const store = createStore(reducers, applyMiddleware(thunk));
 ReactDOM.render(
   <Provider store={store}>
